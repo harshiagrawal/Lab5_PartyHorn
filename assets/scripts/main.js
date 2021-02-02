ae9dccb65aea79cc8e299e13fd3af5cbb68ea53a
harshiagrawal <57237288+harshiagrawal@users.noreply.github.com>
2021-02-02 15:34:00 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,11 @@
 // main.js
 
 // TODO
+
+var inputNum  = document.getElementById("volume-number")
+inputNum.onclick = function() {sliderVolume()};
+
+function sliderVolume(){
+  document.getElementById("volume-slider").value = inputNum;
+}
+
